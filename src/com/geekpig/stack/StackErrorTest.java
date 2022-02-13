4234038ad6ea7250f2984335e849dd1bc4c3ff2a
zflovcoding <2043166212@qq.com>
2022-02-13 22:44:47 +0800

    stack over flow

---
+++ src/com/geekpig/stack/StackErrorTest.java
@@ -0,0 +1,10 @@
+package com.geekpig.stack;
+
+public class StackErrorTest {
+    private static int count = 1;
+    public static void main(String[] args) {
+        System.out.println(count);
+        count++;
+        main(args);
+    }
+}
